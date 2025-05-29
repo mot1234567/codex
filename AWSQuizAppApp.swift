0a1f082d5d1e04db85e4e6b1a0e4770e498f8782
mot1234567 <moto0605_yyy@yahoo.co.jp>
2025-05-29 17:35:16 +0900

    ignore macOS & Xcode user files

---
+++ AWSQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWSQuizAppApp.swift
+//  AWSQuizApp
+//
+//  Created by 藤田基紘 on 2025/05/27.
+//
+
+import SwiftUI
+
+@main
+struct AWSQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
